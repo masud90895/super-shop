a3f7572d991dd9584d6b337120217f0daf4006b7
masud90895 <masudhossainmbs129@gmail.com>
2022-12-25 23:27:41 +0600

    footer complete

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -14,17 +14,18 @@ const Footer = () => {
         <p
           className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500 sm:mx-0 sm:max-w-xs sm:text-left"
         >
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
-          consequuntur amet culpa cum itaque neque.
+           Super Shop online for selling clothing, fashion, high fashion, men fashion, women fashion accessories, brands, perfumes, sarees, tops, shoes and more.
         </p>
 
         <ul className="mt-8 flex justify-center gap-6 sm:justify-start md:gap-8">
           <li>
             <a
+            title='Facebook'
+            href='https://www.facebook.com/masud90895'
                
               rel="noreferrer"
               target="_blank"
-              className="text-teal-700 transition hover:text-teal-700/75"
+              className="transition "
             >
               <span className="sr-only">Facebook</span>
               <svg
@@ -44,33 +45,11 @@ const Footer = () => {
 
           <li>
             <a
-               
+            href='https://twitter.com/masud90895'
+               title='Twitter'
               rel="noreferrer"
               target="_blank"
-              className="text-teal-700 transition hover:text-teal-700/75"
-            >
-              <span className="sr-only">Instagram</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-
-          <li>
-            <a
-               
-              rel="noreferrer"
-              target="_blank"
-              className="text-teal-700 transition hover:text-teal-700/75"
+              className="transition "
             >
               <span className="sr-only">Twitter</span>
               <svg
@@ -88,10 +67,11 @@ const Footer = () => {
 
           <li>
             <a
-               
+            href='http://github.com/masud90895'
+               title='Github'
               rel="noreferrer"
               target="_blank"
-              className="text-teal-700 transition hover:text-teal-700/75"
+              className="text-black transition "
             >
               <span className="sr-only">GitHub</span>
               <svg
@@ -111,24 +91,14 @@ const Footer = () => {
 
           <li>
             <a
-               
+            href='https://www.linkedin.com/in/masud90895/'
+               title='Linkedin'
               rel="noreferrer"
               target="_blank"
               className="text-teal-700 transition hover:text-teal-700/75"
             >
               <span className="sr-only">Dribbble</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
             </a>
           </li>
         </ul>
@@ -278,7 +248,7 @@ const Footer = () => {
 
           <ul className="mt-8 space-y-4 text-sm">
             <li>
-              <a
+              <h1
                 className="flex items-center justify-center gap-1.5 sm:justify-start"
                  
               >
@@ -297,12 +267,12 @@ const Footer = () => {
                   />
                 </svg>
 
-                <span className="text-gray-700">john@doe.com</span>
-              </a>
+                <a href="mailto:masudhossainmbs129@gmail.com" rel='noreferrer' target={'_blank'}>masudhossainmbs129@gmail.com</a>
+              </h1>
             </li>
 
             <li>
-              <a
+              <h1
                 className="flex items-center justify-center gap-1.5 sm:justify-start"
                  
               >
@@ -321,8 +291,8 @@ const Footer = () => {
                   />
                 </svg>
 
-                <span className="text-gray-700">0123456789</span>
-              </a>
+                <a href="mailto:+8801821286135" rel='noreferrer' target={'_blank'}>+8801821286135</a>
+              </h1>
             </li>
 
             <li
@@ -349,7 +319,7 @@ const Footer = () => {
               </svg>
 
               <address className="-mt-0.5 not-italic text-gray-700">
-                213 Lane, London, United Kingdom
+               Thakurgaon Sadar,Bangladesh
               </address>
             </li>
           </ul>
@@ -380,7 +350,7 @@ const Footer = () => {
         </p>
 
         <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
-          &copy; 2022 Company Name
+          &copy; 2022 Md Mahafujur Rahaman Masud
         </p>
       </div>
     </div>
