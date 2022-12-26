47ec60ebc8f3d2aca4f454d480e02eff2f5d667d
masud90895 <masudhossainmbs129@gmail.com>
2022-12-26 17:10:45 +0600

    navbar mobail and tab responsive added

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -8,12 +8,15 @@ import orderTrack from "../../Assists/tracking.svg";
 
 const Navbar = () => {
   const [language, setLanguages] = useState(false);
+  const [show, setShow] = useState(true);
   return (
     <div className="">
       <div className="bg-gray-200 lg:block hidden ">
         <div className=" flex font-normal py-2 justify-evenly text-black mx-[350px]">
-          <Link className="hover:text-purple-900">SAVE MORE ON APP</Link>
-          <Link className="hover:text-purple-900">BECOME A MERCHANT</Link>
+          <label className="hover:text-purple-900" htmlFor="my-modal">
+            SAVE MORE ON APP
+          </label>
+          <Link className="hover:text-purple-900">BECOME A SELLER</Link>
           <Link className="hover:text-purple-900">CAMPAIGN</Link>
           <Link className="hover:text-purple-900">TRACK MY ORDER</Link>
           <button
@@ -22,7 +25,7 @@ const Navbar = () => {
           >
             {language ? "English" : "বাংলা"}
           </button>
-          <Link className="hover:text-purple-900">PRESS RELEASE</Link>
+          <Link className="hover:text-purple-900">ABOUT US</Link>
           <div className="flex items-center gap-2 text-green-700">
             <span>
               <HiOutlinePhoneMissedCall className="zoom-in-out-box" />
@@ -100,7 +103,12 @@ const Navbar = () => {
       <div className="lg:hidden mx-2 mt-2">
         <div className=" flex  justify-between">
           {/* drower  */}
+
+          
           <div className="flex items-center gap-2">
+            {/* test  */}
+          <div className="relative">
+            <div className=" flex justify-between border rounded" onClick={() => setShow(!show)}>
             <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -117,6 +125,69 @@ const Navbar = () => {
                 />
               </svg>
             </div>
+               
+            </div>
+            {show && (
+                <ul className="visible z-50 transition duration-300 opacity-100 bg-white dark:bg-gray-800  shadow rounded mt-2 py-1 w-48 absolute">
+                    <li className="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-book" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                            <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                            <line x1={3} y1={6} x2={3} y2={19} />
+                            <line x1={12} y1={6} x2={12} y2={19} />
+                            <line x1={21} y1={6} x2={21} y2={19} />
+                        </svg>
+                        <span className="ml-2 font-normal">SAVE MORE ON APP</span>
+                    </li>
+                    <li className="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-clipboard" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M9 5H7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2V7a2 2 0 0 0 -2 -2h-2" />
+                            <rect x={9} y={3} width={6} height={4} rx={2} />
+                        </svg>
+                        <span className="ml-2 font-normal">BECOME A SELLER</span>
+                    </li>
+                    <li className="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-compass" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <polyline points="8 16 10 10 16 8 14 14 8 16" />
+                            <circle cx={12} cy={12} r={9} />
+                        </svg>
+                        <span className="ml-2 font-normal">CAMPAIGN</span>
+                    </li>
+                    <li>
+                        <hr className="border-gray-200 dark:border-gray-700 my-1" />
+                    </li>
+                    <li className="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 flex items-center font-normal">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-device-mobile" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <rect x={7} y={4} width={10} height={16} rx={1} />
+                            <line x1={11} y1={5} x2={13} y2={5} />
+                            <line x1={12} y1={17} x2={12} y2="17.01" />
+                        </svg>
+                        <span className="ml-2">TRACK MY ORDER</span>
+                    </li>
+                    <li className="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 flex items-center font-normal">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-tag" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M11 3L20 12a1.5 1.5 0 0 1 0 2L14 20a1.5 1.5 0 0 1 -2 0L3 11v-4a4 4 0 0 1 4 -4h4" />
+                            <circle cx={9} cy={9} r={2} />
+                        </svg>
+                        <span className="ml-2">ABOUT US</span>
+                    </li>
+                    <li className="cursor-pointer text-gray-600 dark:text-gray-400 text-sm leading-3 tracking-normal py-3 hover:bg-gray-100 px-3 flex items-center font-normal">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width={16} height={16} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <line x1={18} y1={6} x2={6} y2={18} />
+                            <line x1={6} y1={6} x2={18} y2={18} />
+                        </svg>
+                        <span className="ml-2">Sign Out</span>
+                    </li>
+                </ul>
+            )}
+        </div>
+          {/* end  */}
             {/* logo  */}
             <div>
               <img className="h-10" src={companyLogo} alt="" />
