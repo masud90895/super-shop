c63b4abf5e055ef5c45a2ebfcfe856ea8a905f0d
masud90895 <masudhossainmbs129@gmail.com>
2023-10-24 14:19:46 +0600

    redux connected by store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { api } from './api/api'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 })
+
+setupListeners(store.dispatch)
