55bb065fe22fa009ad7d328fd55b6b141704b968
masud90895 <masudhossainmbs129@gmail.com>
2023-10-26 21:38:11 +0600

    debug added

--- src/Outlet/Main.js
+++ src/Outlet/Main.js
@@ -4,6 +4,8 @@ import ButtomContactButton from "../Components/Shared/ButtomContactButton";
 import Footer from "../Components/Shared/Footer";
 import Navbar from "../Components/Shared/Navbar";
 
+console.log("object");
+
 const Main = () => {
   return (
     <div>
